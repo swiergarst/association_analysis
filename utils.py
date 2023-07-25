f85587b1b60156b0f8a5678c41f219a82d1b48d8
swiergarst <swier.garst@gmail.com>
2023-07-25 11:51:24 +0200

    working version on leiden

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 # we might want to try different initializations later on
-def init_global_params(data_cols, extra_cols):
+def init_global_params(data_cols, extra_cols, param_seed = 42):
     if None in extra_cols:
         extra_len = 0
     elif "Sens_1" in extra_cols or "Sens_2" in extra_cols:
@@ -10,7 +10,9 @@ def init_global_params(data_cols, extra_cols):
     else:
         extra_len = len(extra_cols)
     model_len = len(data_cols) + extra_len
-    return np.random.normal(0, 1, model_len), [np.random.normal(0, 1)]
+    s = np.random.default_rng(seed=param_seed)
+    return s.normal(0, 1, model_len), [s.normal(0,1)]
+    #return np.random.normal(0, 1, model_len), [np.random.normal(0, 1)]
 
 
 # simple fedAvg implementation
@@ -26,7 +28,7 @@ def average(params, sizes):
 
     #do averaging
     parameters = np.zeros(params.shape[0], dtype=float)
-    print(weights.shape, params.shape, parameters.shape)
+    #print(weights.shape, params.shape, parameters.shape)
     for j in range(num_clients):
         parameters += weights[j] * params[:,j]
 
