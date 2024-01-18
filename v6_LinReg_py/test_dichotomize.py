2d95f5fedeb7f819b54781a1be7a7f5bae9a13cc
swiergarst <swier.garst@gmail.com>
2024-01-18 09:31:18 +0100

    changed dichotomization behavior, now drops the middle category (so the two categories left are highest and lowest)

--- v6_LinReg_py/test_dichotomize.py
+++ v6_LinReg_py/test_dichotomize.py
@@ -3,7 +3,7 @@ import numpy as np
 
 sample_input = np.array([1, 2, 0, 2, 0, 1])
 
-ohe = OneHotEncoder(categories=[[0,1, 2]], sparse=False, drop='first')
+ohe = OneHotEncoder(categories=[[0,1, 2]], sparse=False, drop=[1])
 
 mapped_arr = ohe.fit_transform(sample_input.reshape(-1, 1))
 
